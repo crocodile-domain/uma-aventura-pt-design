d53eb74a2ace463aee3bac8d6dd2fa79348e8ba5
Patricia Carvalho <patricia.carvalho@me.com>
2024-04-16 20:44:02 +0100

    Criação de Style Guide basica e inicio da homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: ["public/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'orange': '#ff903e',
+      'yellow': '#ffd450',
+      'green': '#82c557',
+      'red': '#a1334f',
+      'bred': '#e40035',
+      'bdred': '#ab3b30',
+      'borange': '#e96800',
+      'byellow': '#eddf0b',
+      'bpink': '#e1588e',
+      'bblue': '#009edb',
+      'bgreen': '#6d9802',
+      'blgreen': '#9dce59',
+      'lgrey': '#efefef',
+      },
+    extend: {
+      fontFamily: {
+        brandfont: "'Outfit', sans-serif",
+      },
+      backgroundImage: {
+        'angle': "url('/public/img/angle2.svg')",
+      }
+      
+    },
   },
   plugins: [],
 }
